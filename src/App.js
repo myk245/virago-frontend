c003e4008ccad84385704e05485c847aacd09320
myk245 <myk245@nyu.edu>
2020-04-25 21:59:57 -0400

    first fetch successfully made

--- src/App.js
+++ src/App.js
@@ -3,26 +3,42 @@ import './App.css';
 import './Styles.css';
 import { Route, Switch } from 'react-router-dom';
 import Home from './Components/Home';
-import MainContainer from './Containers/MainContainer';
-import Auth from './Containers/Auth';
+// import MainContainer from './Containers/MainContainer';
+// import Auth from './Containers/Auth';
 import SignUp from './Components/SignUp';
 import LogIn from './Components/LogIn';
 import NavBar from './Components/NavBar';
 import DisordersContainer from './Containers/DisordersContainer';
 import PostsContainer from './Containers/PostsContainer';
+import { API_BASE } from './constants';
 
 class App extends React.Component {
+  state = {
+    disorders: []
+  }
+
+  componentDidMount() {
+    fetch(`${API_BASE}/disorders`)
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({
+          disorders: data 
+        })
+      })
+  }
+
   render() {
+    console.log(this.state.disorders)
     return (
         <div className="App">
           <NavBar />
-        <Switch>
-          <Route path='/signup' component={SignUp} />
-          <Route path='/login' component={LogIn} />
-          <Route path='/disorders' component={DisordersContainer} />
-          <Route path='/posts' component={PostsContainer} />
-          <Route exact path='/' component={Home} />
-        </Switch>
+          <Switch>
+            <Route path='/signup' component={SignUp} />
+            <Route path='/login' component={LogIn} />
+            <Route path='/disorders' render={routerProps => <DisordersContainer {...routerProps} disorders={this.state.disorders} />} />
+            <Route path='/posts' component={PostsContainer} />
+            <Route exact path='/' component={Home} />
+          </Switch>
         </div>
     );
   }
