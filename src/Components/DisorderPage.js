917c12c6a50e76149de7c431d0a2004680b6096a
myk245 <myk245@nyu.edu>
2020-04-27 13:35:22 -0400

    disorder show page done

--- src/Components/DisorderPage.js
+++ src/Components/DisorderPage.js
@@ -16,10 +16,18 @@ class DisorderPage extends React.Component {
       }))
    }
    
-   renderDisorderDetails = () => {
+   renderDisorderDetails = (selectedDisorder) => {
       return (
-         <div>
+         <div id="disorder-page">
             <h1>{this.state.disorder.name}</h1>
+            <p>{this.state.disorder.details}</p>
+            <ul><strong>Common symptoms may include:</strong></ul>
+            {this.state.disorder.symptoms.map(symptom => <li><strong>{symptom.name}:</strong> {symptom.description}</li>)}
+            <br></br>
+            <p><strong>Effective treatment plans are unique to an individual and should always be
+            developed in collaboration with a mental health specialist. Below is a list of
+            treatments commonly used to treat this disorder:</strong></p>
+            {this.state.disorder.treatments.map(treatment => (<li><strong>{treatment.name}:</strong> {treatment.description}</li>))}
          </div>
       ) 
    }
@@ -28,7 +36,7 @@ class DisorderPage extends React.Component {
       console.log(this.state.disorder)
       return (
          <div>
-            {/* {this.renderDisorderDetails()} */}
+            {this.state.disorder && this.renderDisorderDetails(this.state.disorder)}
          </div>
       )
    }
