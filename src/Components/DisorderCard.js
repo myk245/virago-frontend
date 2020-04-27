08e534af02c90a08ad180d4f726ec0a625a9b0f8
myk245 <myk245@nyu.edu>
2020-04-27 02:38:07 -0400

    started with rendering of show page

--- src/Components/DisorderCard.js
+++ src/Components/DisorderCard.js
@@ -4,15 +4,18 @@ import Button from 'react-bootstrap/Button';
 import { useHistory } from 'react-router-dom'; 
 
 const DisorderCard = (props) => {
+   let history = useHistory(); 
+
    return (
          <Card id="disorder-card" className="text-center">
             <Card.Header>{props.disorder.name}</Card.Header>
                <Card.Body>
-               <Card.Title><strong></strong></Card.Title>
+               {/* <Card.Title><strong></strong></Card.Title> */}
                <Card.Text>
                   {props.disorder.details}
-               </Card.Text>
-            <Button variant="info" onClick={() => props.handleSelect(props.disorder.id)}>Details</Button>
+            </Card.Text>
+            <Button variant="info" onClick={() => history.push(`/disorders/${props.disorder.id}`)}>Details</Button>
+            {/* <Button variant="info" onClick={() => props.handleSelect(props.disorder.id)}>Details</Button> */}
             </Card.Body>
          </Card>
    )
--- src/Components/DisorderPage.js
+++ src/Components/DisorderPage.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { API_BASE } from '../constants'; 
 
-const DisorderPage = () => {
-   return (
-      <div>
-         
-      </div>
-   )
+class DisorderPage extends React.Component {
+   state = {
+      disorder: null
+   }
+
+   componentDidMount() {
+      // console.log(this.props) //=> routerProps
+      let id = this.props.match.params.id
+      fetch(API_BASE + `/disorders/${id}`)
+         .then(resp => resp.json())
+         .then(data => this.setState({
+            disorder: data 
+      }))
+   }
+   
+   renderDisorderDetails = () => {
+      return (
+         <div>
+            <h1>{this.state.disorder.name}</h1>
+         </div>
+      ) 
+   }
+
+   render() {
+      console.log(this.state.disorder)
+      return (
+         <div>
+            {/* {this.renderDisorderDetails()} */}
+         </div>
+      )
+   }
 }
+
 
 export default DisorderPage; 
