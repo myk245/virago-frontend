9eadffc94d48c94bc29cfe522a707e714e17c8c0
myk245 <myk245@nyu.edu>
2020-05-07 01:22:47 -0400

    minor styling changes

--- src/Components/Resources.js
+++ src/Components/Resources.js
@@ -8,7 +8,7 @@ const Resources = () => {
          <Card className="center-container" style={{ width: '60rem' }} border="info">
             <Card.Header class="text-primary"><h3>Resources</h3></Card.Header>
             <Card.Body>
-               <Card.Subtitle class="text-muted">If you or someone you know is struggling with a mental health disorder, the below resources are helpful starting points for getting more information and finding the help you need. <em>For urgent or emergency help, please contact 911.</em></Card.Subtitle>
+               <Card.Subtitle class="text-muted">If you or someone you know is struggling with a mental health disorder, the below resources are helpful starting points for getting more information and finding the help you need. <em><strong>For urgent or emergency help, please contact 911.</strong></em></Card.Subtitle>
                <br></br>
                <ListGroup variant="flush">
                   <ListGroup.Item><strong><Card.Link href="https://www.nami.org/Home">National Alliance on Mental Illness</Card.Link></strong>: "the nation's largest grassroots mental health organization dedicated to building better lives for the millions of Americans affected by mental illness."</ListGroup.Item>
