ecfe7a8afca22672f65b6903466a33088206e952
myk245 <myk245@nyu.edu>
2021-10-08 09:03:04 -0400

    modify home screen

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -10,13 +10,13 @@ class Home extends React.Component {
          <div id="home-div">
             <br></br>
          <Jumbotron id="jumbotron">
-            <h1 class="text-primary">Hello, friend! Welcome to Virago.</h1>
+            <h1 class="text-primary">Welcome to Virago.</h1>
             <br></br>
             <p class="text-dark">Virago is a centralized resource and community support group for mental health care. Explore general mental health wellness tips, get further information on specific disorders, and interact with a community of compassionate people who understand what you're going through. 
             </p>
             <p><em>No matter what you're going through, you're not alone.</em></p>
             <p>
-               <Button variant="primary" href="/disorders">Start Exploring Virago</Button>
+               <Button variant="primary" href="/disorders">Start Exploring</Button>
             </p>
             </Jumbotron>
             <Card border="info" id="general-info-card">
